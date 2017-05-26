ded9ecdc62be554f6aba9d8cbcbd945fc97b7dcd
ahmad <ahmad@ahmad-All-Series>
2017-05-26 13:33:19 +0700

    Merge origin/master
    Conflicts:
    	web/page-opentiket-form.html

--- web/js/PageOpenTicket.js
+++ web/js/PageOpenTicket.js
@@ -7,18 +7,32 @@
 
 
 $(document).ready(function () {
-
+    var data_atm;
 
     $.post('helper_atm', {code: "0"},
             function (returnedData) {
-             
-                var arr = $.parseJSON(returnedData);
-                  console.log(returnedData);
-                for (var i = 0; i <= arr.length; i++) {
-                    console.log(arr["id_atm"]);                    
-                };
+                var obj = JSON.parse(returnedData);
+                data_atm = JSON.parse(returnedData);
+                console.log(returnedData);
+                jQuery.each(obj, function (i, val) {
+                    console.log(obj[i]["id_atm"]);
+                    $('<option>').val(obj[i]["id_atm"]).text(obj[i]["id_atm"]).appendTo('#select_atm');
+                });
+                ;
 
             }).fail(function () {
         console.log("error");
+    });
+
+    $("#select_atm").click(function () {
+        var id_atm = $("#select_atm").val();
+        jQuery.each(data_atm, function (i, val) {
+            console.log(data_atm[i]["id_atm"]);
+            if (data_atm[i]["id_atm"]=== id_atm){
+                //nih disini 
+                   console.log(data_atm[i]["nama_lokasi"]+" "+ data_atm[i]["p_terakhir"]+" "+data_atm[i]["nama_atm"]+" "+ data_atm[i]["kordinator"]);
+            }
+
+        });
     });
 });
